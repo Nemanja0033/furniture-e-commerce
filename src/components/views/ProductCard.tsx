23ca7ce0ce512f769919b9d02ae4ef789ae931db
nemanja <antonijevicnemanja68@gmail.com>
2025-02-20 00:43:43 +0100

    Hover animations for card

--- src/components/views/ProductCard.tsx
+++ src/components/views/ProductCard.tsx
@@ -8,10 +8,10 @@ interface ProductCardProps {
 const ProductCard = ({img, title, price}: ProductCardProps) => {
 
 	return(
-		<div className="md:w-[300px] w-[90%] h-[400px] rounded-md grid grid-col-1 shadow-md">
+		<div className="md:w-[300px] w-[90%] hover:opacity-80 translation-all cursor-pointer h-[400px] rounded-md grid grid-col-1 shadow-md">
 			<img className="h-[90%] w-full" src={img} alt={title} />
 			<span className="text-xl text-center font-semibold">{title}</span>
-			<span className="text-center">{price}$</span>
+			<span className="text-center font-semibold">{price}$</span>
 		</div>
 	)
 }
