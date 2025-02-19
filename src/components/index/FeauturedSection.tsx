1951bac391b63074ef271aed8ed8b6f46dd3c3ea
Nemanja <antonijevicnemanja68@gmail.com>
2025-02-19 12:23:03 +0100

    Products Card component & data displaying dynamic

--- src/components/index/FeauturedSection.tsx
+++ src/components/index/FeauturedSection.tsx
@@ -1,14 +1,17 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
+import ProductCard from '../views/ProductCard';
+import { ArrowRight } from 'lucide-react';
+import { Link } from 'react-router';
 
 const FeauturedSection = () => {
-	const [products, setProducts] = useState([]);
+	const [products, setProducts] = useState<any[]>([]);
 	console.log(products)
 
 	useEffect(() => {
-		axios.get('https://furniture-api.fly.dev/v1/products')
+		axios.get('https://furniture-api.fly.dev/v1/products/?&limit=8')
 		.then((res) => {
-			setProducts(res.data);
+			setProducts(res.data.data);
 		})
 		.catch((err) => {
 			console.log(err);
@@ -18,8 +21,18 @@ const FeauturedSection = () => {
 	return(
 		<main className="w-full h-auto md:text-start text-center font-light px-10 flex-row">
 			<h3 className="text-xl">Aks Collection</h3>
-			<h1 className="font-semibold text-2xl">Feautured Products</h1>
-
+			<div className='flex justify-between'>
+				<h1 className="font-semibold text-2xl">Feautured Products</h1>
+				<Link to={'/products'} className='flex gap-2 cursor-pointer'>View all <ArrowRight /></Link>
+			</div>
+			<section className='w-full grid md:grid-cols-4 grid-cols-1 mt-3 gap-3'>
+			{products.map((p: any) => (
+					<ProductCard img={p.image_path} 
+								 title={p.name} 
+								 price={p.price} 
+								 />
+			))}
+			</section>
 		</main>
 	)
 }
--- src/components/views/ProductCard.tsx
+++ src/components/views/ProductCard.tsx
@@ -1,12 +1,19 @@
 
 interface ProductCardProps {
 	img: string, 
-	title: string
+	title: string,
+	price: number
 }
 
-const ProductCard = ({img, title}: ProductCardProps) => {
+const ProductCard = ({img, title, price}: ProductCardProps) => {
 
 	return(
-
+		<div className="w-[300px] h-[400px] border border-gray-200 rounded-md grid grid-col-1 shadow-md p-1">
+			<img className="h-[90%] w-full" src={img} alt={title} />
+			<span className="text-xl text-center font-semibold">{title}</span>
+			<span className="text-center">{price}$</span>
+		</div>
 	)
 }
+
+export default ProductCard;
