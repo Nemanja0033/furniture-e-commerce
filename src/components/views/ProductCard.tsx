2cd383c8beb0c1c89d6d332c313b84797b441bc0
nemanja <antonijevicnemanja68@gmail.com>
2025-02-22 01:47:02 +0100

    fix ui product card

--- src/components/views/ProductCard.tsx
+++ src/components/views/ProductCard.tsx
@@ -8,7 +8,7 @@ interface ProductCardProps {
 const ProductCard = ({img, title, price}: ProductCardProps) => {
 
 	return(
-		<div className="md:w-[300px] w-[90%] hover:opacity-80 translation-all cursor-pointer h-[400px] rounded-md grid grid-col-1 shadow-md">
+		<div className="md:w-[100%] w-[90%] hover:opacity-80 translation-all cursor-pointer h-[400px] rounded-md grid grid-col-1 shadow-md">
 			<img className="h-[90%] w-full" src={img} alt={title} />
 			<span className="text-xl text-center font-semibold px-1">{title}</span>
 			<span className="text-center font-semibold">{price}$</span>
