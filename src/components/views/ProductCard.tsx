00c918e66364e0e184735178c421ba1fd6613418
nemanja <antonijevicnemanja68@gmail.com>
2025-02-24 14:18:37 +0100

    adjust product card size

--- src/components/views/ProductCard.tsx
+++ src/components/views/ProductCard.tsx
@@ -24,7 +24,7 @@ const ProductCard = ({img, title, price, desc, wood_type, id}: ProductCardProps)
 	}
 
 	return(
-		<div onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)} className={`md:w-72 w-full hover:opacity-80 translation-all cursor-pointer h-full rounded-md grid grid-col-1 shadow-md`}>
+		<div onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)} className={`lg:w-72 w-full hover:opacity-80 translation-all cursor-pointer h-full rounded-md grid grid-col-1 shadow-md`}>
 			<img className="h-72 w-full" src={img} alt={title} />
 			<button onClick={() => dispatch({type: "ADD_ITEM", payload: item})} className={`rounded-lg relative ${isHover ? 'opacity-100' : 'opacity-0'} m-0 h-8 translition-all bottom-15 left-3 bg-white flex justify-center border-gray-300 border p-1 hover:bg-gray-100 cursor-pointer w-10`}><ShoppingCart /></button>
 			<div className="relative bottom-5">
