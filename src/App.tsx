2db59174fc57e0ba82603cb097994c88cb9e4993
Nemanja <antonijevicnemanja68@gmail.com>
2025-02-24 13:33:39 +0100

    setting folder structure & react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router"
 
 function App() {
 
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        
+      </Routes>
+    </Router>
   )
 }
 
