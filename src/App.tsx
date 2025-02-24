d2d948038163bc618ff963adc274078c5e5cc74c
Nemanja <antonijevicnemanja68@gmail.com>
2025-02-24 13:39:49 +0100

    setting routes and paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router"
+import Index from "./pages/Index"
+import Products from "./pages/Products"
+import SingleProduct from "./pages/SingleProduct"
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        
+        <Route path="/" element={<Index />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/product/:id" element={<SingleProduct />} />
       </Routes>
     </Router>
   )
