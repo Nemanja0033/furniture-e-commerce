ac700a2d5bd0479b684a99bc0f89085d6f1e7d67
nemanja <antonijevicnemanja68@gmail.com>
2025-02-24 13:53:26 +0100

    cart counter display

--- src/components/nav/Navigation.tsx
+++ src/components/nav/Navigation.tsx
@@ -28,7 +28,8 @@ const Navigation = () => {
             : 
             null
             }
-            <button onClick={() => setIsCartOpen(true)} className='rounded-lg border-gray-300 border p-1 hover:bg-gray-100 cursor-pointer'><ShoppingCart size={24} />{cartItems[0].length}</button>
+            <button onClick={() => setIsCartOpen(true)} className='rounded-lg border-gray-300 border p-1 hover:bg-gray-100 cursor-pointer'><ShoppingCart size={24} /></button>
+            <span className='bg-slate-500 text-white rounded-full w-6 scale-90 relative right-5 bottom-3 text-center'>{cartItems[0].length}</span>
         </div>
     </nav>
   )
