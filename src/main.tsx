20f2430c2f98eb06315cf40e67907da19375004a
Nemanja <antonijevicnemanja68@gmail.com>
2025-03-01 22:49:38 +0100

    fix FilterContext path

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { FilterProvider } from './context/FilterReducer.tsx'
+import { FilterProvider } from './context/FliterContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
