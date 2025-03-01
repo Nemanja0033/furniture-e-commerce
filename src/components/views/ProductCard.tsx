d39a51a23d81c38a97e2fce6130390a69a447370
Nemanja <antonijevicnemanja68@gmail.com>
2025-03-01 23:55:27 +0100

    state for hover feautures

--- src/components/views/ProductCard.tsx
+++ src/components/views/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { ArrowRight, ShoppingCart } from "lucide-react";
 import { useCart } from "../../context/CartContext";
 import { Link } from "react-router";
+import { useState } from "react";
 
 interface ProductCardProps {
 	img: string, 
@@ -11,12 +12,14 @@ interface ProductCardProps {
 };
 
 const ProductCard = ({img, title, price, desc, wood_type}: ProductCardProps) => {
+	const [isHover, setIsHover] = useState(false);
+	console.log(isHover)
 	const { dispatch } = useCart();
 
 	return(
-		<div className="w-72 hover:opacity-80 translation-all cursor-pointer h-full rounded-md grid grid-col-1 shadow-md">
+		<div onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)} className={`w-72 hover:opacity-80 translation-all cursor-pointer h-full rounded-md grid grid-col-1 shadow-md`}>
 			<img className="h-72 w-full" src={img} alt={title} />
-			<button className='rounded-lg relative bottom-10 left-3 bg-white flex justify-center border-gray-300 border p-1 hover:bg-gray-100 cursor-pointer w-10'><ShoppingCart /></button>
+			<button className={`rounded-lg ${isHover ? 'relative' : 'hidden'} bottom-10 left-3 bg-white flex justify-center border-gray-300 border p-1 hover:bg-gray-100 cursor-pointer w-10`}><ShoppingCart /></button>
 			<Link to={'/'} className="text-lg mt-1 hover:underline text-start font-light px-5">{title}</Link>
 			<div className="my-2 px-5 justify-start text-light text-gray-600 flex">
 				<p>{desc}, {wood_type}</p>
