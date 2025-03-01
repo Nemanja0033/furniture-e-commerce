88c4cdf4be69bd83adeec8552c349ee427c81cbf
Nemanja <antonijevicnemanja68@gmail.com>
2025-03-01 23:48:10 +0100

    cart btn ui

--- src/components/views/ProductCard.tsx
+++ src/components/views/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, ShoppingCart } from "lucide-react";
 import { useCart } from "../../context/CartContext";
 import { Link } from "react-router";
 
@@ -16,6 +16,7 @@ const ProductCard = ({img, title, price, desc, wood_type}: ProductCardProps) => 
 	return(
 		<div className="w-72 hover:opacity-80 translation-all cursor-pointer h-full rounded-md grid grid-col-1 shadow-md">
 			<img className="h-72 w-full" src={img} alt={title} />
+			<button className='rounded-lg relative bottom-10 left-3 bg-white flex justify-center border-gray-300 border p-1 hover:bg-gray-100 cursor-pointer w-10'><ShoppingCart /></button>
 			<Link to={'/'} className="text-lg mt-1 hover:underline text-start font-light px-5">{title}</Link>
 			<div className="my-2 px-5 justify-start text-light text-gray-600 flex">
 				<p>{desc}, {wood_type}</p>
