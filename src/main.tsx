db42470519b2ab31e0b2b85ab358172bc7051feb
Nemanja <antonijevicnemanja68@gmail.com>
2025-03-01 22:51:48 +0100

    wrap app component with cart provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { FilterProvider } from './context/FliterContext.tsx'
+import { CartProvider } from './context/CartContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FilterProvider>
-      <App />
-    </FilterProvider>
+    <CartProvider>
+      <FilterProvider>
+        <App />
+      </FilterProvider>
+    </CartProvider>
   </StrictMode>,
 )
